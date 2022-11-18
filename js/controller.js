7388f7dfd8901600cddca8904e8dc5c6ecc62e77
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-11-18 12:45:05 +0100

    Getränke-Challenge II

--- js/controller.js
+++ js/controller.js
@@ -1,39 +1,38 @@
-
 /* Controller ***
     Ablaufsteuerung 
     Business-Logic (Alter --> Getränk) :: check! 
 */
 
 //Modul: Ablaufsteuerung (control) --> Test:
-//controller();
-function controller() {
-    
-    // actions:
-    let ageNum = getInput();
-    let bevObj = checkAge(ageNum);
-    let isImgLoaded = updateImg(bevObj.bevArr);
-    let isTxtupdated = updateTxt(bevObj.bevArr);
+    //controller();
+    function controller() {
+        
+        // actions:
+        let ageNum = getInput();
+        let bevObj = checkAge( ageNum );
+        let isImgLoaded = updateImg( bevObj.bevArr );
+        let isTxtupdated = updateTxt( bevObj.bevArr );
 
-    // monitoring
-    ausgabe("Getränke: " + bevObj.bevArr); // Alters-Check
-    ausgabe(isImgLoaded); // Img
-    ausgabe(isTxtupdated);
-}
+        // monitoring
+        ausgabe( "Getränke: " + bevObj.bevArr );    // Alters-Check
+        ausgabe( isImgLoaded );                     // Img
+        ausgabe( isTxtupdated );
+    }
 
 //Funktionalität: Business-Logic --> Test:
-//ausgabe(checkAge(32));
-function checkAge(age) {
-    switch (true) {
-        case age >= data.milk.lower && age <= data.milk.upper:
-            return { bevStr: data.milk.bev, bevArr: data.milk.arr };
-        case age >= data.juice.lower && age <= data.juice.upper:
-            return { bevStr: data.juice.bev, bevArr: data.juice.arr };
-        case age >= data.cola.lower && age <= data.cola.upper:
-            return { bevStr: data.cola.bev, bevArr: data.cola.arr };
-        case age >= data.wine.lower && age <= data.wine.upper:
-            return { bevStr: data.wine.bev, bevArr: data.wine.arr };
-        default:
-            return { bevStr: data.tea.bev, bevArr: data.tea.arr };
+    //ausgabe(checkAge(32));
+    function checkAge( age ) {
+        switch ( true ) {
+            case age >= data.milk.lower && age <= data.milk.upper:
+                return { bevStr: data.milk.bev, bevArr: data.milk.arr };
+            case age >= data.juice.lower && age <= data.juice.upper:
+                return { bevStr: data.juice.bev, bevArr: data.juice.arr };
+            case age >= data.cola.lower && age <= data.cola.upper:
+                return { bevStr: data.cola.bev, bevArr: data.cola.arr };
+            case age >= data.wine.lower && age <= data.wine.upper:
+                return { bevStr: data.wine.bev, bevArr: data.wine.arr };
+            default:
+                return { bevStr: data.tea.bev, bevArr: data.tea.arr };
+        }
     }
-}
 
